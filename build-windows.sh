2fb14de3df5c30807abe3f20495f417a40951663
steven <stevengriffin323@gmail.com>
2014-10-25 00:24:17 -0400

    updated Windows build file

--- build-windows.sh
+++ build-windows.sh
@@ -3,4 +3,9 @@
 java -jar packr.jar packr-config-windows.json
 
 cp CREDITS.md LICENSE.txt README.md out-windows/
+
+# Windows .exe doesn't seem to work, so use a .bat file.
+cp cruciform.bat out-windows/
+rm out-windows/cruciform.exe
+
 zip -r cruciform-windows out-windows
