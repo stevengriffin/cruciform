d4e0904b63fe82fd2f71e8fe4ac06b81bda44b6c
steven <stevengriffin323@gmail.com>
2014-10-24 20:47:38 -0400

    added license file

--- build-all.sh
+++ build-all.sh
@@ -3,12 +3,12 @@
 # Pack all jars with jdk 8 for users without jdk 8 installed.
 java -jar packr.jar packr-config-linux64.json
 
-# Copy credits file to each folder.
-cp CREDITS.md out-linux64/
-cp CREDITS.md out-linux32/
-cp CREDITS.md out-mac/
-cp CREDITS.md out-windows/
-cp CREDITS.md out-jar/
+# Copy some files to each folder.
+cp CREDITS.md LICENSE.txt README.md out-linux64/
+cp CREDITS.md LICENSE.txt README.md out-linux32/
+cp CREDITS.md LICENSE.txt README.md out-mac/
+cp CREDITS.md LICENSE.txt README.md out-windows/
+cp CREDITS.md LICENSE.txt README.md out-jar/
 
 # Zip all builds
 zip -r cruciform-linux64 out-linux64
